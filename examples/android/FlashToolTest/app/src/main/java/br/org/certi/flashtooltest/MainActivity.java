e2beb578fbcad6f6ecec04ca7eb839b8bd0222a5
Ian Koerich Maciel <inm@certi.org.br>
2018-05-11 17:03:39 -0300

    Fix Android permission and Android manifest for Microbit board
    The Android manifest file was registering the wrong vendor/product id (it was in hex while Android expects decimal values).
    Now the app won't need to ask permission while running.
    
    Also, the example application is now receiving a callback when a compatible board is connected and the list of devices is refreshed.

--- examples/android/FlashToolTest/app/src/main/java/br/org/certi/flashtooltest/MainActivity.java
+++ examples/android/FlashToolTest/app/src/main/java/br/org/certi/flashtooltest/MainActivity.java
@@ -15,13 +15,20 @@
  */
 package br.org.certi.flashtooltest;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import br.org.certi.jocd.board.MbedBoard;
+import br.org.certi.jocd.dapaccess.dapexceptions.InsufficientPermissions;
 import br.org.certi.jocd.tools.AsyncResponse;
 import java.util.List;
 
@@ -32,6 +39,24 @@ public class MainActivity extends AppCompatActivity implements
   private static final String TAG = "MainActivity";
 
   TextView textViewConnectedBoards;
+  TextView textViewResult;
+
+  private String ACTION_USB_PERMISSION;
+  private static final String ACTION_USB_ATTACHED = "android.hardware.usb.action.USB_DEVICE_ATTACHED";
+  private static final String ACTION_USB_DETACHED = "android.hardware.usb.action.USB_DEVICE_DETACHED";
+  private PendingIntent permissionIntent;
+  IntentFilter filter;
+
+  private static enum Fsm {
+    INIT,
+    CLICKED_LIST_DEVICES,
+    STARTED_LISTING_DEVICES,
+    CLICKED_FLASH_DEVICE,
+    STARTED_FLASHING_DEVICE,
+    TESTS_ACTIVITY
+  }
+
+  Fsm fsm = Fsm.INIT;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +64,31 @@ public class MainActivity extends AppCompatActivity implements
     setContentView(R.layout.activity_main);
 
     textViewConnectedBoards = (TextView) findViewById(R.id.textViewConnectedBoards);
+    textViewResult = (TextView) findViewById(R.id.textViewResult);
     textViewConnectedBoards.setText("Devices....");
+
+    // Register the broadcast receiver to request permission for USB devices.
+    ACTION_USB_PERMISSION = this.getPackageName() + ".USB_PERMISSION";
+    permissionIntent = PendingIntent.getBroadcast(this, 0,
+        new Intent(ACTION_USB_PERMISSION), 0);
+    filter = new IntentFilter();
+    filter.addAction(ACTION_USB_PERMISSION);
+    filter.addAction(ACTION_USB_ATTACHED);
+    filter.addAction(ACTION_USB_DETACHED);
+    registerReceiver(broadcastReceiver, filter);
+
+    // Update the list of devices.
+    listDevices();
   }
 
   public void onClickListDevices(View view) {
     Log.d("CLICK", "Button list devices clicked.");
+    this.fsm = Fsm.CLICKED_LIST_DEVICES;
+    listDevices();
+  }
 
+  private void listDevices() {
+    this.fsm = Fsm.STARTED_LISTING_DEVICES;
     // Create a new async task to list all connected devices.
     AsyncFlashToolListDevices asyncListDevices = new AsyncFlashToolListDevices(this, this);
     asyncListDevices.execute();
@@ -52,14 +96,21 @@ public class MainActivity extends AppCompatActivity implements
 
   public void onClickFlashDevice(View view) {
     Log.d("CLICK", "Button flash device clicked.");
+    this.fsm = Fsm.CLICKED_FLASH_DEVICE;
+    flashDevice();
+  }
 
-    // Create a new async task to list all connected devices.
+  private void flashDevice() {
+    this.fsm = Fsm.STARTED_FLASHING_DEVICE;
+    // Create a new async task to flash the first devices.
     AsyncFlashToolFlashBoard asyncFlashDevice = new AsyncFlashToolFlashBoard(this, this);
     asyncFlashDevice.execute();
   }
 
   public void onClickTestsButon(View view) {
     Log.d("CLICK", "Button open tests clicked.");
+
+    this.fsm = Fsm.TESTS_ACTIVITY;
 
     Intent intent = new Intent(this, TestsActivity.class);
     startActivity(intent);
@@ -76,25 +127,86 @@ public class MainActivity extends AppCompatActivity implements
    * Callback to onPostExecute (from AsyncTask.FlashBoard).
    */
   public void processAsyncTaskFinish(String result) {
-    textViewConnectedBoards.setText(result);
+    textViewResult.setText(result);
+    this.fsm = Fsm.INIT;
   }
 
   /*
    * Callback to onPostExecute (from AsyncTask.ListBoards).
    */
   public void processAsyncTaskFinish(List<MbedBoard> boards) {
-    String boardsText = "";
-    for (int i  = 0; i < boards.size(); i++) {
-      MbedBoard board = boards.get(i);
-      boardsText = boardsText + "Board " + i + ": " + board.name + " (Board ID: " + board.boardId + ")\n";
+
+    if (boards == null || boards.size() == 0) {
+      textViewConnectedBoards.setText("No boards connected.");
+      this.fsm = Fsm.INIT;
+      return;
     }
+
+    String boardsText = "";
+    for (int i = 0; i < boards.size(); i++) {
+      MbedBoard board = boards.get(i);
+      boardsText =
+          boardsText + "Board " + (i + 1) + ": " + board.name + " (Board ID: " + board.boardId
+              + ")\n";
+    }
+
     textViewConnectedBoards.setText(boardsText);
+    this.fsm = Fsm.INIT;
   }
 
   /*
    * Callback to onException (from AsyncTask.ListBoards/FlashBoard).
    */
   public void processAsyncException(Exception exception) {
-    // TODO
+    if (exception instanceof InsufficientPermissions) {
+      // We don't have access to access the USB device.
+      // Request permission.
+
+      UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+      UsbDevice usbDevice = (UsbDevice) ((InsufficientPermissions) exception).usbDevice;
+      usbManager.requestPermission(usbDevice, permissionIntent);
+
+      Log.w(TAG, "Insufficient permissions to access USB Device");
+      return;
+    }
+  }
+
+  private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+    public void onReceive(Context context, Intent intent) {
+      String action = intent.getAction();
+      if (ACTION_USB_PERMISSION.equals(action)) {
+        synchronized (this) {
+          UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+          if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+            if (device != null) {
+              resumeOperation();
+            }
+          } else {
+            Log.d(TAG, "Permission denied for device.");
+            textViewConnectedBoards.setText("Permission denied for device.");
+          }
+        }
+      }
+
+      if (ACTION_USB_ATTACHED.equals(action) || ACTION_USB_DETACHED.equals(action)) {
+        listDevices();
+      }
+    }
+  };
+
+  private void resumeOperation() {
+
+    if (fsm == Fsm.CLICKED_LIST_DEVICES || fsm == Fsm.STARTED_LISTING_DEVICES) {
+      // Restart.
+      // Create a new async task to list all connected devices.
+      listDevices();
+    }
+
+    if (fsm == Fsm.CLICKED_LIST_DEVICES || fsm == Fsm.STARTED_FLASHING_DEVICE) {
+      // Restart.
+      // Create a new async task to list all connected devices.
+      flashDevice();
+    }
   }
 }
