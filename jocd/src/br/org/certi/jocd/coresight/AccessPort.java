062b96ea301204227b4f3d6989a5fc3a6bdbc45b
Elisa de Freitas Kuhlkamp <efk@certi.org.br>
2018-05-17 15:04:10 -0300

    Implement Acess Port

---
+++ jocd/src/br/org/certi/jocd/coresight/AccessPort.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2018 Fundação CERTI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package br.org.certi.jocd.coresight;
+
+import br.org.certi.jocd.coresight.DebugPort.AP_REG;
+import br.org.certi.jocd.dapaccess.DapAccessCmsisDap;
+import br.org.certi.jocd.dapaccess.Transfer;
+import br.org.certi.jocd.dapaccess.dapexceptions.Error;
+import br.org.certi.jocd.dapaccess.dapexceptions.TransferError;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+public class AccessPort {
+
+  public static final byte AP_ROM_TABLE_ADDR_REG = (byte) 0xf8;
+  public static final byte AP_ROM_TABLE_FORMAT_MASK = 0x2;
+  public static final byte AP_ROM_TABLE_ENTRY_PRESENT_MASK = 0x1;
+
+
+  // Map from PIDR to component name (eventually class).
+  public static final Map<Long, Long> MEM_AP_IDR_TO_WRAP_SIZE;
+
+  static {
+    MEM_AP_IDR_TO_WRAP_SIZE = new HashMap<Long, Long>();
+    MEM_AP_IDR_TO_WRAP_SIZE.put(0x24770011L,
+        0x1000L);// Used on m4 & m3 - Documented in arm_cortexm4_processor_trm_100166_0001_00_en.pdf and arm_cortexm3_processor_trm_100165_0201_00_en.pdf
+    MEM_AP_IDR_TO_WRAP_SIZE
+        .put(0x44770001L, 0x400L);// Used on m1 - Documented in DDI0413D_cortexm1_r1p0_trm.pdf
+    MEM_AP_IDR_TO_WRAP_SIZE.put(0x04770031L, 0x400L);// Used on m0+? at least on KL25Z, KL46, LPC812
+    MEM_AP_IDR_TO_WRAP_SIZE.put(0x04770021L, 0x400L);// Used on m0? used on nrf51, lpc11u24
+    MEM_AP_IDR_TO_WRAP_SIZE.put(0x64770001L, 0x400L);// Used on m7
+    MEM_AP_IDR_TO_WRAP_SIZE.put(0x74770001L, 0x400L);// Used on m0+ on KL28Z
+  }
+
+  // AP Control and Status Word definitions
+  public static final long CSW_SIZE = 0x00000007;
+  public static final long CSW_SIZE8 = 0x00000000;
+  public static final long CSW_SIZE16 = 0x00000001;
+  public static final long CSW_SIZE32 = 0x00000002;
+  public static final long CSW_ADDRINC = 0x00000030;
+  public static final long CSW_NADDRINC = 0x00000000;
+  public static final long CSW_SADDRINC = 0x00000010;
+  public static final long CSW_PADDRINC = 0x00000020;
+  public static final long CSW_DBGSTAT = 0x00000040;
+  public static final long CSW_TINPROG = 0x00000080;
+  public static final long CSW_HPROT = 0x02000000;
+  public static final long CSW_MSTRTYPE = 0x20000000;
+  public static final long CSW_MSTRCORE = 0x00000000;
+  public static final long CSW_MSTRDBG = 0x20000000;
+  public static final long CSW_RESERVED = 0x01000000;
+
+  public static final long CSW_VALUE = (CSW_RESERVED | CSW_MSTRDBG | CSW_HPROT | CSW_DBGSTAT
+      | CSW_SADDRINC);
+
+  public static final Map<Integer, Long> TRANSFER_SIZE;
+
+  static {
+    TRANSFER_SIZE = new HashMap<Integer, Long>();
+    TRANSFER_SIZE.put(8, CSW_SIZE8);
+    TRANSFER_SIZE.put(16, CSW_SIZE16);
+    TRANSFER_SIZE.put(32, CSW_SIZE32);
+  }
+
+  // Debug Exception and Monitor Control Register
+  public static final long DEMCR = 0xE000EDFC;
+  // DWTENA in armv6 architecture reference manual
+  public static final long DEMCR_TRCENA = (1 << 24);
+
+  public DebugPort dp;
+  public int apNum;
+  public DapAccessCmsisDap link;
+  public long idr = 0;
+  public long romAddr = 0;
+  public boolean hasRomTable = false;
+  public RomTable romTable;
+  public boolean initedPrimary = false;
+  public boolean initedSecondary = false;
+
+  public long getRomAddr() {
+    return romAddr;
+  }
+
+  public AccessPort(DebugPort dp, int apNum) {
+    this.dp = dp;
+    this.apNum = apNum;
+    this.link = dp.getLink();
+  }
+
+  public void init(Boolean busAccessible) throws Exception {
+    if (!this.initedPrimary) {
+      this.idr = this.readRegNow(AP_REG.IDR.getValue());
+
+      // Init ROM table
+      this.romAddr = this.readRegNow(AP_ROM_TABLE_ADDR_REG);
+      this.hasRomTable =
+          (this.romAddr != 0xFFFFFFFF) && ((this.romAddr & AP_ROM_TABLE_ENTRY_PRESENT_MASK) != 0);
+      // Clear format and present bits
+      this.romAddr &= 0xFFFFFFFC;
+      this.initedPrimary = true;
+    }
+
+    if (!this.initedSecondary && this.hasRomTable && busAccessible) {
+      this.initRomTable();
+      this.initedSecondary = true;
+    }
+  }
+
+  public void initRomTable() throws Exception {
+    this.romTable = new RomTable(this);
+    this.romTable.init();
+  }
+
+  public long readRegNow(long addr) throws Exception {
+    return this.dp.readAPNow(((this.apNum << DebugPort.APSEL_SHIFT) | addr));
+  }
+
+  public ArrayList<Object> readRegLater(long addr) throws Exception {
+    return this.dp.readAP(((this.apNum << DebugPort.APSEL_SHIFT) | addr));
+  }
+
+  public void writeReg(long addr, Long data) throws TimeoutException, Error {
+    this.dp.writeAP((this.apNum << DebugPort.APSEL_SHIFT) | addr, data);
+  }
+
+  public void writeMemory(long addr, long data) throws TimeoutException, Error {
+    throw new InternalError("Not implemented");
+  }
+
+  public void writeMemory(long addr, long data, int transferSize) throws Error, TimeoutException {
+    throw new InternalError("Not implemented");
+  }
+
+  public long readMemory(long address, Integer transferSize) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public long readMemoryNow(long addr, Integer transferSize) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public ArrayList<Object> readMemoryLater(long addr, Integer transferSize) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public long readMemoryAsync(Transfer transfer, int numDp, long addr, Integer transferSize,
+      int num)
+      throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public void writeBlock32(long addr, long[] word) throws TimeoutException, Error {
+    throw new InternalError("Not implemented");
+  }
+
+  public long[] readBlock32(long addr, int size) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public void write32(long addr, long value) throws TimeoutException, Error {
+    throw new InternalError("Not implemented");
+  }
+
+  public void write16(long addr, long value) throws TimeoutException, Error {
+    throw new InternalError("Not implemented");
+  }
+
+  public void write8(long addr, long value) throws TimeoutException, Error {
+    throw new InternalError("Not implemented");
+  }
+
+  public long read32(long addr) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public int read16(long addr) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public byte read8(long addr) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public byte[] readBlockMemoryUnaligned8(long addr, int size) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public void writeBlockMemoryUnaligned8(long addr, byte[] data) throws TimeoutException, Error {
+    throw new InternalError("Not implemented");
+  }
+
+  public void writeBlockMemoryAligned32(long addr, long[] data) throws TimeoutException, Error {
+    throw new InternalError("Not implemented");
+  }
+
+  public long[] readBlockMemoryAligned32(long addr, int size) throws Exception {
+    throw new InternalError("Not implemented");
+  }
+
+  public void handleError(Error error, int num) throws TransferError, TimeoutException {
+    throw new InternalError("Not implemented");
+  }
+}
---
+++ jocd/src/br/org/certi/jocd/coresight/RomTable.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2018 Fundação CERTI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package br.org.certi.jocd.coresight;
+
+import br.org.certi.jocd.util.Mask;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RomTable extends CoreSightComponent {
+
+  // Logging
+  private final static String CLASS_NAME = RomTable.class.getName();
+  private final static Logger LOGGER = Logger.getLogger(CLASS_NAME);
+
+  private RomTable parent;
+  private long number;
+  private long entrySize = 0;
+  private ArrayList<CoreSightComponent> components = new ArrayList<CoreSightComponent>();
+
+  public RomTable(AccessPort ap) {
+    this(ap, null, null);
+  }
+
+  public RomTable(AccessPort ap, long addr) {
+    this(ap, addr, null);
+  }
+
+  public RomTable(AccessPort ap, RomTable parentTable) {
+    this(ap, null, parentTable);
+  }
+
+  public RomTable(AccessPort ap, Long topAddress, RomTable parentTable) {
+    // If no table address is provided, use the root ROM table for the AP.
+    if (topAddress == null) {
+      topAddress = ap.getRomAddr();
+    }
+    super.setup(ap, topAddress);
+    this.parent = parentTable;
+    this.number = this.parent != null ? (this.parent.number + 1) : 0;
+  }
+
+  public void init() throws Exception {
+    this.readIdRegisters();
+    if (!this.isRomTable) {
+      LOGGER.log(Level.WARNING, String
+          .format("Warning: ROM table @ 0x%08x has unexpected CIDR component class (0x%x)",
+              this.address, this.componentClass));
+      return;
+    }
+    if (this.count4kb != 1) {
+      LOGGER.log(Level.WARNING, String
+          .format("Warning: ROM table @ 0x%08x is larger than 4kB (%d 4kb pages)", this.address,
+              this.count4kb));
+    }
+    this.readTable();
+  }
+
+  public void readTable() throws Exception {
+    LOGGER.log(Level.INFO, String
+        .format("ROM table #%d @ 0x%08x cidr=%x pidr=%x", this.number, this.address, this.cidr,
+            this.pidr));
+    this.components = new ArrayList<CoreSightComponent>();
+
+    // Switch to the 8-bit table entry reader if we already know the entry size.
+    if (this.entrySize == 8) {
+      this.readTable8();
+    }
+
+    long entryAddress = this.address;
+    boolean foundEnd = false;
+    int entriesRead = 0;
+    while (!foundEnd && entriesRead < ROM_TABLE_ENTRY_READ_COUNT) {
+      // Read several entries at a time for performance.
+      int readCount = Math.min(ROM_TABLE_MAX_ENTRIES - entriesRead, ROM_TABLE_ENTRY_READ_COUNT);
+      long[] entries = this.ap.readBlockMemoryAligned32(entryAddress, readCount);
+      entriesRead += readCount;
+
+      // Determine entry size if unknown.
+      if (this.entrySize == 0) {
+        if ((entries[0] & ROM_TABLE_32BIT_MASK) != 0) {
+          this.entrySize = 32;
+        } else {
+          this.entrySize = 8;
+        }
+        if (this.entrySize == 8) {
+          // Read 8-bit table.
+          this.readTable8();
+          return;
+        }
+      }
+
+      for (long entry : entries) {
+        // Zero entry indicates the end of the table.
+        if (entry == 0) {
+          foundEnd = true;
+          break;
+        }
+        this.handleTableEntry(entry);
+
+        entryAddress += 4;
+      }
+    }
+  }
+
+  public void readTable8() throws Exception {
+    long entryAddress = this.address;
+
+    while (true) {
+      // Read the full 32-bit table entry spread across four bytes.
+      long entry = this.ap.read8(entryAddress);
+      entry |= this.ap.read8(entryAddress + 4) << 8;
+      entry |= this.ap.read8(entryAddress + 8) << 16;
+      entry |= this.ap.read8(entryAddress + 12) << 24;
+
+      // Zero entry indicates the end of the table.
+      if (entry == 0) {
+        break;
+      }
+      this.handleTableEntry(entry);
+
+      entryAddress += 16;
+    }
+  }
+
+  public void handleTableEntry(long entry) throws Exception {
+    // Nonzero entries can still be disabled, so check the present bit before handling.
+    if ((entry & ROM_TABLE_ENTRY_PRESENT_MASK) != 0) {
+      return;
+    }
+
+    // Get the component's top 4k address.
+    long offset = entry & ROM_TABLE_ADDR_OFFSET_MASK;
+    if ((entry & ROM_TABLE_ADDR_OFFSET_NEG_MASK) != 0) {
+      offset = ~Mask.invert32(offset);
+    }
+    address = this.address + offset;
+
+    // Create component instance.
+    CoreSightComponent cmp = new CoreSightComponent();
+    cmp.setup(this.ap, address);
+    cmp.readIdRegisters();
+
+    LOGGER.log(Level.INFO, String.format("[%d]%s", this.components.size(), cmp.toString()));
+
+    // Recurse into child ROM tables.
+    if (cmp.isRomTable) {
+      RomTable romTableCmp = new RomTable(this.ap, address, this);
+      romTableCmp.init();
+    }
+    this.components.add(cmp);
+  }
+}
