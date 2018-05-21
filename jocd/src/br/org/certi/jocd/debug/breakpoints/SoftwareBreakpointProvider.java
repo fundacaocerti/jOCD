b60d7f78c44ee522bf1c0558cdfb9e7a85144c05
Ian Koerich Maciel <inm@certi.org.br>
2018-05-21 17:46:13 -0300

    Implement BreakpointManager and remove TODO's
    This commit remove most of TODO's of the code, and in some cases throws an exception where the TODO comment was.
    
    Also implement filterMemory method from BreakpoinManager which was marked as TODO.

--- jocd/src/br/org/certi/jocd/debug/breakpoints/SoftwareBreakpointProvider.java
+++ jocd/src/br/org/certi/jocd/debug/breakpoints/SoftwareBreakpointProvider.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class SoftwareBreakpointProvider extends BreakpointProvider {
 
   private Target core;
-  private List<Breakpoint> Breakpoints = new ArrayList<Breakpoint>();
+  private List<Breakpoint> breakpoints = new ArrayList<Breakpoint>();
 
   public SoftwareBreakpointProvider(Target core) {
     super();
@@ -18,5 +18,42 @@ public class SoftwareBreakpointProvider extends BreakpointProvider {
   public void init() {
   }
 
-  // TODO
+  @Override
+  public boolean doFilterMemory() {
+    return true;
+  }
+
+  @Override
+  public long filterMemory(long address, int size, long word) {
+    for (Breakpoint bp : this.breakpoints) {
+      switch (size) {
+        case 16:
+          if (bp.address == address) {
+            word = bp.originalInstr;
+          }
+          break;
+
+        case 32:
+          if (bp.address == address) {
+            word = (word & 0xFFFF0000L) | bp.originalInstr;
+          } else if (bp.address == address + 2) {
+            word = (word & 0xFFFFL) | (bp.originalInstr << 16);
+          }
+          break;
+      }
+    }
+    return word;
+  }
+
+  @Override
+  public byte filterMemory(long address, byte data) {
+    for (Breakpoint bp : this.breakpoints) {
+      if (bp.address == address) {
+        data = (byte) (bp.originalInstr & 0xFFL);
+      } else if (bp.address + 1 == address) {
+        data = (byte) (bp.originalInstr >> 8);
+      }
+    }
+    return data;
+  }
 }
